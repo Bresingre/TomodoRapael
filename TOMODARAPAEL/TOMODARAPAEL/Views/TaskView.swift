d9fc41d2f4324a20ed2f1d503eb957b3e2b37ce3
bresingre <axeljoue@gmail.com>
2024-03-13 11:54:49 +0100

    rectangle

--- TOMODARAPAEL/TOMODARAPAEL/Views/TaskView.swift
+++ TOMODARAPAEL/TOMODARAPAEL/Views/TaskView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct TaskView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.orange)
+            
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.blue)
+        }
     }
+    
 }
 
 struct TaskView_Previews: PreviewProvider {
