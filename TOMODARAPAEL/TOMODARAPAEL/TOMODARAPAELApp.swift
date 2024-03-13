e0603c2f7b52b766e622141db6e7cade5afadf42
TheGreatestRock <vic38100@gmail.com>
2024-03-13 10:46:35 +0100

    update

--- TOMODARAPAEL/TOMODARAPAEL/TOMODARAPAELApp.swift
+++ TOMODARAPAEL/TOMODARAPAEL/TOMODARAPAELApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TOMODARAPAELApp: App {
     var body: some Scene {
         WindowGroup {
-            TomodoRapael()
+            Rappel()
                 .environmentObject(TaskViewModel())
         }
     }
--- TOMODARAPAEL/TOMODARAPAEL/Views/RowView.swift
+++ TOMODARAPAEL/TOMODARAPAEL/Views/RowView.swift
@@ -12,28 +12,22 @@ struct RowView: View {
     let task: Tasks
     let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
-        formatter.dateFormat = "YY/MM/dd"
+        formatter.dateFormat = "dd/MM/YY"
         return formatter
     }()
     
     var body: some View {
-        HStack {
-            Image(systemName: task.isCompleted ? "checkmark.circle" : "circle")
-                .foregroundColor(task.isCompleted ? .green : .red)
-            Text(dateFormatter.string(from: task.date)) // Using dateFormatter here
-            Text(task.title)
-            
-            Spacer()
-            
-            Text(task.category.rawValue)
-                .font(.footnote)
-                .padding(3)
-                .foregroundStyle(Color(.systemGray2))
-                .frame(minWidth: 62)
-                .overlay(
-                    Capsule().stroke(Color(.systemGray2),
-                            lineWidth:0.75)
-                )
+        VStack{
+            HStack{
+                Text(task.title)
+                Spacer()
+            }
+            HStack {
+                Text(dateFormatter.string(from: task.date))
+                Image(systemName: task.isCompleted ? "checkmark.square" : "square")
+                    .foregroundColor(task.isCompleted ? .green : .red)
+                
+            }
         }
         .font(.title2)
         .padding(.vertical, 8)
