08bc4960d1f6d6a05e4a9755ad6a2a905ac52af7
Bresingre <axeljoue@gmail.com>
2024-03-14 13:34:35 +0100

    page principale

---
+++ TOMODARAPAEL/TOMODARAPAEL/Views/Rappel.swift
@@ -0,0 +1,14 @@
+//
+//  Rappel.swift
+//  TOMODARAPAEL
+//
+//  Created by joue axel on 14/03/2024.
+//
+
+import SwiftUI
+
+struct Rappel: View {
+    var body: some View{
+        Text("rappel vue")
+    }
+}
