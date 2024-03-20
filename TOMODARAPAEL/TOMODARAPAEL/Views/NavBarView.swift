458a2b3453d10a5adb5d89c0b44346f3eab2ec58
TheGreatestRock <vic38100@gmail.com>
2024-03-20 11:45:29 +0100

    t

--- TOMODARAPAEL/TOMODARAPAEL/Views/NavBarView.swift
+++ TOMODARAPAEL/TOMODARAPAEL/Views/NavBarView.swift
@@ -9,52 +9,71 @@ import SwiftUI
 
 struct RoundedSquareButton: View {
     var text: String
+    var destination: AnyView
     
     var body: some View {
-        Text(text)
-            .frame(width: 60, height: 30)
-            .foregroundColor(.white)
-            .background(Color.blue)
-            .cornerRadius(10)
+        NavigationLink(destination: destination) {
+            Text(text)
+                .frame(width: 60, height: 30)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(10)
+        }
     }
 }
 
 struct CircleButton: View {
     var image: Image
+    var destination: AnyView
     
     var body: some View {
-        image
-            .resizable()
-            .frame(width: 50, height: 50)
-            .background(Color.white)
-            .foregroundColor(Color.blue)
-            .clipShape(Circle())
-    }
-}
- 
-struct Navbar: View {
-    var body: some View {
-        HStack(spacing: 10) {
-            Spacer()
-            RoundedSquareButton(text: "Home")
-            RoundedSquareButton(text: "Alarm")
-            CircleButton(image: Image(systemName: "person.circle.fill"))
-            RoundedSquareButton(text: "Share")
-            RoundedSquareButton(text: "Pet")
-            Spacer()
+        NavigationLink(destination: destination) {
+            image
+                .resizable()
+                .frame(width: 50, height: 50)
+                .background(Color.white)
+                .foregroundColor(Color.blue)
+                .clipShape(Circle())
         }
-        .padding()
-        
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        Navbar()
+        NavigationView {
+            Navbar()
+        }
     }
 }
 
-struct NavBarView_Previews: PreviewProvider {
+struct Navbar: View {
+    var body: some View {
+        HStack(spacing: 10) {
+            Spacer()
+            RoundedSquareButton(text: "Home", destination: AnyView(Rappel()))
+            RoundedSquareButton(text: "Alarm", destination: AnyView(TaskView()))
+            CircleButton(image: Image(systemName: "person.circle.fill"), destination: AnyView(ProfilView()))
+            RoundedSquareButton(text: "Share", destination: AnyView(ShareView()))
+            RoundedSquareButton(text: "Pet", destination: AnyView(PetView()))
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct ShareView: View {
+    var body: some View {
+        Text("Share View")
+    }
+}
+
+struct PetView: View {
+    var body: some View {
+        Text("Pet View")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
