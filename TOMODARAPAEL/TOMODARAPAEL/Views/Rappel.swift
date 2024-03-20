35b0bc82383446101f6f492a2592879de77a939d
TheGreatestRock <vic38100@gmail.com>
2024-03-20 10:21:46 +0100

    e

--- TOMODARAPAEL/TOMODARAPAEL/Views/Rappel.swift
+++ TOMODARAPAEL/TOMODARAPAEL/Views/Rappel.swift
@@ -4,17 +4,54 @@
 //
 //  Created by joue axel on 14/03/2024.
 //
-
 import SwiftUI
 
 struct Rappel: View {
+    
+    @EnvironmentObject var taskViewModel: TaskViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack(alignment: .bottomTrailing) {
+                List {
+                    ForEach(taskViewModel.tasks) { task in
+                        RowView(task: task)
+                            .onTapGesture {
+                                taskViewModel.updateItem(task: task)
+                            }
+                    }
+                    .onDelete(perform: taskViewModel.deleteItem)
+                    .onMove(perform: taskViewModel.moveItem)
+                }
+                .listStyle(PlainListStyle())
+                .navigationTitle("Rappel")
+                
+                VStack(alignment: .trailing, spacing: 5) {
+                    Text("Category")
+                        .font(.headline)
+                        .foregroundColor(.primary)
+                        .padding()
+                        .background(Color.white) // Add white background here
+                    ForEach(Category.allCases) { category in
+                        HStack {
+                            Image(systemName: "largecircle.fill.circle" )
+                                .foregroundColor(category.color())
+                                .font(.title)
+                            Text(category.rawValue)
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    .padding(.trailing, 10)
+                }
+                .padding()
+            }
+        }
     }
 }
 
-struct Rappel_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Rappel()
+        Rappel().environmentObject(TaskViewModel())
     }
 }
